b11af726de739a15784cea59682f75cd3bb7afdd
Kobayashi Takahiro <koba925@gmail.com>
2024-12-15 17:51:37 +0900

    四則演算を実装する

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ fn main() {
                 } else {
                     match word {
                         "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
                         _ => panic!("{word:?} coud not be parsed"),
                     }
                 }
@@ -21,8 +24,25 @@ fn main() {
 }
 
 fn add(stack: &mut Vec<i32>) {
-    let lhs = stack.pop().unwrap();
     let rhs = stack.pop().unwrap();
-
+    let lhs = stack.pop().unwrap();
     stack.push(lhs + rhs);
+}
+
+fn sub(stack: &mut Vec<i32>) {
+    let rhs = stack.pop().unwrap();
+    let lhs = stack.pop().unwrap();
+    stack.push(lhs - rhs);
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let rhs = stack.pop().unwrap();
+    let lhs = stack.pop().unwrap();
+    stack.push(lhs * rhs);
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let rhs = stack.pop().unwrap();
+    let lhs = stack.pop().unwrap();
+    stack.push(lhs / rhs);
 }
